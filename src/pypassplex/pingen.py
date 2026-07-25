4fef1c5b9a283f029e3832cabf69bb4c6891059f
LukeS-05 <238086390+LukeS-05@users.noreply.github.com>
2026-07-25 18:20:06 +0100

    Bump version

--- src/pypassplex/pingen.py
+++ src/pypassplex/pingen.py
@@ -18,9 +18,9 @@ def generate(length=6):
     
 def entropy(length=6):
     if not(isinstance(length, int)):
-        raise TypeError(f"[236] (pingen@PPK v{__version__}) - PIN length must be given as an integer.")
+        raise TypeError(f"[236] (pingen@PPX v{__version__}) - PIN length must be given as an integer.")
     
     if length < 1:
-        raise ValueError(f"[237] (pingen@PPK v{__version__}) - Length must be greater than or equal to 1 for a PIN.")
+        raise ValueError(f"[237] (pingen@PPX v{__version__}) - Length must be greater than or equal to 1 for a PIN.")
     entropy = passgen.entropy(pool=string.digits, length=length)
     return entropy
