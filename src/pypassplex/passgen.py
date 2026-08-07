ba5185326a88c01c8cb4a7d46504a05a3cc8b798
LukeS-05 <238086390+LukeS-05@users.noreply.github.com>
2026-08-07 12:18:31 +0100

    feature: Replaced EFF short words list with long words list

--- src/pypassplex/passgen.py
+++ src/pypassplex/passgen.py
@@ -14,7 +14,7 @@ __all__ = ["generate", "buildPool", "entropy"]
 def buildPool(upper=True, lower=True, numbers=True, symbols=True):
     # 1 - CHECK BOOLEAN VALUES HAVE BEEN PASSED
     if not(isinstance(upper, bool)) or not(isinstance(lower, bool)) or not(isinstance(numbers, bool)) or not(isinstance(symbols, bool)):
-        raise TypeError(f"[201] (passgen@PPK v{__version__}) - Character pool config (e.g. upper) must be given as booleans.")
+        raise TypeError(f"[201] (passgen@PPX v{__version__}) - Character pool config (e.g. upper) must be given as booleans.")
 
     # 2 - INITIALISE CHARACTERS VARIABLE
     characters = ""
@@ -30,14 +30,14 @@ def buildPool(upper=True, lower=True, numbers=True, symbols=True):
 
 def generate(upper=True, lower=True, numbers=True, symbols=True, length=10, returnPool=False):
     # 1 - DATA TYPE VALIDATION
-    if not(isinstance(length, int)): raise TypeError(f"[202] (passgen@PPK v{__version__}) - Password length must be given as an integer.")
+    if not(isinstance(length, int)): raise TypeError(f"[202] (passgen@PPX v{__version__}) - Password length must be given as an integer.")
     # 2 - BUILD CHARACTER POOL
     characters = buildPool(upper, lower, numbers, symbols)
     selected = sum([upper, lower, numbers, symbols])
     
     # 3 - VALIDATE LENGTH AND SELECTED CATEGORIES
-    if length < selected: raise ValueError(f"[203] (passgen@PPK v{__version__}) - Length must be greater than or equal to the number of character types selected ({selected}).")
-    if selected == 0: raise ValueError(f"[204] (passgen@PPK v{__version__}) - You must select at least one type of character.")
+    if length < selected: raise ValueError(f"[203] (passgen@PPX v{__version__}) - Length must be greater than or equal to the number of character types selected ({selected}).")
+    if selected == 0: raise ValueError(f"[204] (passgen@PPX v{__version__}) - You must select at least one type of character.")
 
     # 4 - INITIALISE CHARSLIST
     charslist = []
@@ -64,17 +64,17 @@ def generate(upper=True, lower=True, numbers=True, symbols=True, length=10, retu
 def entropy(pool="", length=0):
     # 1 - DATA TYPE VALIDATION
     if not(isinstance(length, int)):
-        raise TypeError(f"[206] (passgen@PPK v{__version__}) - Password length must be given as an integer.")
+        raise TypeError(f"[206] (passgen@PPX v{__version__}) - Password length must be given as an integer.")
     if not(isinstance(pool, str)):
-        raise TypeError(f"[209] (passgen@PPK v{__version__}) - Character pool must be given as a string.")
+        raise TypeError(f"[209] (passgen@PPX v{__version__}) - Character pool must be given as a string.")
 
     # 2 - CHECK POOL HAS BEEN PASSED
     if not pool:
-        raise ValueError(f"[207] (passgen@PPK v{__version__}) - Your character pool cannot be empty.")
+        raise ValueError(f"[207] (passgen@PPX v{__version__}) - Your character pool cannot be empty.")
     
     # 3 - HANDLED NEGATIVE LENGTHS (0.7.1)
     if length < 0:
-        raise ValueError(f"[208] (passgen@PPK v{__version__}) - Length must not be a negative number.")
+        raise ValueError(f"[208] (passgen@PPX v{__version__}) - Length must not be a negative number.")
 
     # 4 - EXCLUDE DUPLICATE CHARS TO ENSURE ENTROPY IS ACCURATE
     poolsize = len(set(pool))
--- src/pypassplex/phrasegen.py
+++ src/pypassplex/phrasegen.py
@@ -16,14 +16,14 @@ __all__ = ["generate","entropy"]
 def wordsList(file=None, wordlist=None):
     # 1 - GENERATE WORDLIST
     # RULE 1 - IF BOTH ARE PASSED, RAISE VALUEERROR
-    if file and wordlist: raise ValueError(f"[212] (phrasegen@PPK v{__version__}) - You must pass EITHER file or wordlist as argument\nBoth have been passed.")
+    if file and wordlist: raise ValueError(f"[212] (phrasegen@PPX v{__version__}) - You must pass EITHER file or wordlist as argument\nBoth have been passed.")
     # RULE 2 - IF NEITHER IS PASSED, USE EFF WORDS LIST
     if not file and not wordlist: 
         try:
             with resources.files("pypassplex").joinpath("eff-words.txt").open("r", encoding="utf-8") as f:
                 words = f.read().splitlines()
         except Exception as e:
-            raise RuntimeError(f"[219] (phrasegen@PPK v{__version__}) {e}")
+            raise RuntimeError(f"[219] (phrasegen@PPX v{__version__}) {e}")
     # RULE 3 - IF ONLY FILE IS PASSED, USE FILE
     elif file:
         try:
@@ -31,11 +31,11 @@ def wordsList(file=None, wordlist=None):
             with open(file, "r", encoding="utf-8") as f:
                 words = f.read().splitlines()
         except FileNotFoundError as e:
-            raise FileNotFoundError(f"[211] (phrasegen@PPK v{__version__}) - File does not exist. - {e}")
+            raise FileNotFoundError(f"[211] (phrasegen@PPX v{__version__}) - File does not exist. - {e}")
     # RULE 4 - IF ONLY WORD LIST IS PASSED, USE WORD LIST
     elif wordlist:
         if not(isinstance(wordlist, list)):
-            raise ValueError(f"[214] (phrasegen@PPK v{__version__}) - Wordlist must be a lists")
+            raise ValueError(f"[214] (phrasegen@PPX v{__version__}) - Wordlist must be a lists")
         words = wordlist
     # 2 - REMOVE SPACES FROM WORD LIST
     words = [w for w in words if w.strip()]
