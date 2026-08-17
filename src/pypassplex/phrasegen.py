b63b12a7f117fc2888e66c9516cb1abf738be98a
LukeS-05 <238086390+LukeS-05@users.noreply.github.com>
2026-08-17 16:01:04 +0100

    refactor(phrasegen): minor improvements to code

--- src/pypassplex/phrasegen.py
+++ src/pypassplex/phrasegen.py
@@ -16,48 +16,42 @@ __all__ = ["generate","entropy"]
 
 # 0.7.1 - NEW WORDSLIST FUNCTION TO REPLACE IDENTICAL CODE IN TWO FUNCTIONS
 def wordsList(file=None, wordlist=None):
-    # 1 - GENERATE WORDLIST
-    # RULE 1 - IF BOTH ARE PASSED, RAISE VALUEERROR
+    # if both are passed
     if file and wordlist: error(ValueError, 212, "phrasegen")
-    # RULE 2 - IF NEITHER IS PASSED, USE EFF WORDS LIST
+    # if neither is passed
     if not file and not wordlist: 
         try:
             with resources.files("pypassplex").joinpath("eff-words.txt").open("r", encoding="utf-8") as f:
                 words = f.read().splitlines()
-        except Exception as e:
-            error(RuntimeError, 219, "phrasegen", e)
-    # RULE 3 - IF ONLY FILE IS PASSED, USE FILE
+        except Exception as e: error(RuntimeError, 219, "phrasegen", e)
+    # if only file is passed
     elif file:
         try:
             # 0.7.1 - FIX FOR UnicodeDecodeError
             with open(file, "r", encoding="utf-8") as f:
                 words = f.read().splitlines()
-        except FileNotFoundError as e:
-            error(FileNotFoundError, 212, "phrasegen", e)
-    # RULE 4 - IF ONLY WORD LIST IS PASSED, USE WORD LIST
+        except FileNotFoundError as e: error(FileNotFoundError, 212, "phrasegen", e)
+    # if only wordlist is passed
     elif wordlist:
         if not(isinstance(wordlist, list)):
             error(ValueError, 214, "phrasegen")
         words = wordlist
     # 2 - REMOVE SPACES FROM WORD LIST
-    words = [w for w in words if w.strip()]
+    cleanedwords = []
+    for i in words:
+        if i.strip(): cleanedwords.append(i.strip())
+    
+    words = list(dict.fromkeys(cleanedwords))
 
-    words = list(dict.fromkeys(words))
+    if not(words): error(ValueError, 215, "phrasegen")
 
-    if not(words):
-        error(ValueError, 215, "phrasegen")
-
-    # 3 - RETURN WORDS
     return words
 
 def generate(file=None, wordlist=None, length=4, delimiter="-", case="lower"):
-    if length <= 0:
-        error(ValueError, 208, "phrasegen")
+    if length <= 0: error(ValueError, 208, "phrasegen")
+    if case not in ["lower", "upper", "title"]: error(ValueError, 218, "phrasegen")
     
     words = wordsList(file, wordlist)
-
-    if case not in ["lower", "upper", "title"]:
-        error(ValueError, 218, "phrasegen")
     
     passphrase = "" # nosec 
     for i in range(length):
