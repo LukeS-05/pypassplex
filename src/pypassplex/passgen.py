3b6d7c7fe13e744e7717b8280f5a5b0a96a97d2f
LukeS-05 <238086390+LukeS-05@users.noreply.github.com>
2026-08-19 21:48:30 +0100

    feat: pypassplex test script

--- src/pypassplex/passgen.py
+++ src/pypassplex/passgen.py
@@ -33,8 +33,10 @@ def validateParameters(upper=True, lower=True, numbers=True, symbols=True, lengt
     selected = sum([upper, lower, numbers, symbols])
                 
     # VALIDATE LENGTH AND SELECTED CATEGORIES
-    if length < selected: error(ValueError, 203, "passgen")
-    if selected == 0: error(ValueError, 204, "passgen")
+    if length < selected: 
+        error(ValueError, 203, "passgen")
+    if selected == 0: 
+        error(ValueError, 204, "passgen")
 
 def satisfiesRequirements(upper, lower, numbers, symbols, candidate):
     hasupper = False
---
+++ src/pypassplex/ppxm/ppx-test.py
@@ -0,0 +1,26 @@
+import pypassplex as ppx
+
+def test(function, arguments, id, shouldPass):
+    try:
+        function(**arguments)
+        executed = True
+        error = ""
+    except Exception as e:
+        executed = False
+        error = e
+    
+    if (not(executed) and not(shouldPass)) or (executed and shouldPass):
+        print(f"Test ID {id} was successful - {error}\nshouldPass = {shouldPass} and executed without error = {executed}")
+    else:
+        print(f"Test ID {id} was not successful - {error}\nshouldPass = {shouldPass} and executed without error = {executed}")
+
+    print("-"*50)
+
+# test all character sets disabled.
+test(ppx.passgen.generate, {
+    "upper": False,
+    "lower": False,
+    "symbols": False,
+    "numbers": False,
+    "length": 10
+}, 102, False)
